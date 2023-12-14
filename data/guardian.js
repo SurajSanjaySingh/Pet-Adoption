f022187a3c1fa0b9883499be0772b0d936fa9bf1
SurajSanjaySingh <singhsurajs1311@gmail.com>
2023-12-14 01:25:35 -0500

    updated guardian

--- data/guardian.js
+++ data/guardian.js
@@ -1,5 +1,5 @@
 import { guardian } from "../config/mongoCollections.js";
-import { isValidName } from "../helpers.js";
+import { isValidName , checkState, checkZip } from "../helpers.js";
 import validator from "validator";
 import PasswordValidator from "password-validator";
 import bcrypt, { hash } from "bcrypt";
@@ -21,7 +21,7 @@ export const createGuardian = async function (
     typeof password === "undefined" &&
     typeof userType === "undefined"
   ) {
-    throw "Error All fields need to have valid values";
+    throw "Error All fields need to have valid values.";
   }
 
   firstName = firstName.trim();
@@ -33,6 +33,47 @@ export const createGuardian = async function (
   if (!(isValidName(firstName) && isValidName(lastName))) {
     throw "Name should be a valid string and should be at least 2 characters long with a max of 25 characters";
   }
+
+  if (typeof location !== 'object'){
+    throw new Error ('Location Must Be Of Object Type.');
+  }
+  if (
+    !location.streetAddress || typeof location.streetAddress !== 'string' ||
+    !location.city || typeof location.city !== 'string' ||
+    !location.state || typeof location.state !== 'string' ||
+    !location.zip || typeof location.zip !== 'string' ){
+      throw new Error ('Zip, State, City And StreetAddress Fileds Should Be Provided And Should Be String Type For location');
+    }
+
+    location.streetAddress = location.streetAddress.trim();
+    location.city = location.city.trim();
+    location.state = location.state.trim();
+    location.zip = location.zip.trim();
+  
+    if (
+        location.streetAddress === " " || 
+        location.city === " " ||
+        location.state === " " ||
+        location.zip === " "){
+        throw new Error ('Zip, State, City And StreetAddress Fileds Should Not Be Empty');
+      }
+    
+    if (location.streetAddress.length < 3){
+      throw new Error ('location.streetAddress Should Not Be Less Than 3 Characters');
+    }
+
+    if (location.city.length < 3){
+      throw new Error ('location.city Should Not Be Less Than 3 Characters');
+    }
+
+    if (!checkState(location.state)){
+      throw new Error ('State Abbreviation Is Not Valid');
+    }
+
+    if (!checkZip(eventLocation.zip)){
+      throw new Error ('Zip Code Is Not Valid');
+    }
+
   if (!validator.isEmail(email))
     throw "Error: not in a valid email address format";
   const collection = await guardian();
